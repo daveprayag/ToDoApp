2bc36e1853fb37624ceca126b98ec0dad9763386
Prayag Dave <prayagdave28@gmail.com>
2023-09-11 13:33:34 +0530

    Changed baseurl in Client

--- client/src/utils/HandleApi.js
+++ client/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://todo-app-daveprayag.vercel.app/";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
