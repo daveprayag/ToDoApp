198027ff2260b0f996cee82296fddfb0314af500
Prayag Dave <prayagdave28@gmail.com>
2023-09-12 09:55:58 +0530

    CORS Error fix 1

--- server/Server.js
+++ server/Server.js
@@ -11,13 +11,7 @@ const app = express();
 const PORT = process.env.PORT | 3000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://todoapp-daveprayag.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://prayagdave:<pdmongodbadmin28..>@cluster0.mnbesg4.mongodb.net/todo?retryWrites=true&w=majority"
